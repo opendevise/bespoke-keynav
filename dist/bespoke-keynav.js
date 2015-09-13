567036649ba0e394e6174df36c3468dd99873666
Dan Allen <dan.j.allen@gmail.com>
2015-09-12 19:22:12 -0600

    remove directionality option; consolidate code & test suite

--- dist/bespoke-keynav.js
+++ dist/bespoke-keynav.js
@@ -6,41 +6,34 @@
  */
 
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g=(g.bespoke||(g.bespoke = {}));g=(g.plugins||(g.plugins = {}));g.keynav = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = function(opts) {
+module.exports = function() {
   return function(deck) {
-    var KEY_SP = 32, KEY_PGUP = 33, KEY_PGDN = 34, KEY_END = 35, KEY_HME = 36,
-        KEY_LT = 37, KEY_UP = 38, KEY_RT = 39, KEY_DN = 40,
-      isHorizontal = opts !== 'vertical',
-      isModifierPressed = function(e) {
-        return !!(e.ctrlKey || e.shiftKey || e.altKey || e.metaKey);
+    var KEY_SB = 32, KEY_PGUP = 33, KEY_PGDN = 34, KEY_END = 35, KEY_HOME = 36, KEY_LFT = 37, KEY_RGT = 39,
+      isModifierPressed = function(e, keyCode) {
+        return e.ctrlKey || (e.shiftKey && keyCode !== KEY_SB) || e.altKey || e.metaKey;
       },
       onKeydown = function(e) {
-        if (e.which === KEY_SP) {
-          if (!e.ctrlKey && !e.altKey && !e.metaKey) return e.shiftKey ? deck.prev() : deck.next();
-        }
-        else if (e.which === KEY_PGDN ||
-            (isHorizontal && e.which === KEY_RT) ||
-            (!isHorizontal && e.which === KEY_DN)) {
-          if (!isModifierPressed(e)) deck.next();
-        }
-        else if (e.which === KEY_PGUP ||
-            (isHorizontal && e.which === KEY_LT) ||
-            (!isHorizontal && e.which === KEY_UP)) {
-          if (!isModifierPressed(e)) deck.prev();
-        }
-        else if (e.which === KEY_END) {
-          var last = deck.slides.length - 1;
-          if (deck.slide() !== last && !isModifierPressed(e)) deck.slide(last);
-        }
-        else if (e.which === KEY_HME) {
-          if (deck.slide() !== 0 && !isModifierPressed(e)) deck.slide(0);
+        var keyCode = e.which;
+        if (isModifierPressed(e, keyCode)) return;
+        switch(keyCode) {
+          case KEY_SB:
+            return e.shiftKey ? deck.prev() : deck.next();
+          case KEY_RGT:
+          case KEY_PGDN:
+            return deck.next();
+          case KEY_LFT:
+          case KEY_PGUP:
+            return deck.prev();
+          case KEY_HOME:
+            return deck.slide(0);
+          case KEY_END:
+            return deck.slide(deck.slides.length - 1);
         }
       };
-
     deck.on('destroy', function() {
-      document.removeEventListener('keydown', onKeydown, false);
+      document.removeEventListener('keydown', onKeydown);
     });
-    document.addEventListener('keydown', onKeydown, false);
+    document.addEventListener('keydown', onKeydown);
   };
 };
 
--- test/spec/bespoke-keynavSpec.js
+++ test/spec/bespoke-keynavSpec.js
@@ -5,10 +5,11 @@ var simulant = require('simulant'),
   keynav = require('../../lib/bespoke-keynav.js'),
   forms = require('bespoke-forms');
 
-describe("bespoke-keys", function() {
-  var deck,
+describe('bespoke-keynav', function() {
+  var KEY = { spaceBar: 32, pageUp: 33, pageDown: 34, end: 35, home: 36, left: 37, right: 39 },
+    deck,
     inputBox = null,
-    createDeck = function(opts) {
+    createDeck = function() {
       var parent = document.createElement('article');
       for (var i = 1; i <= 5; i++) {
         var slide = document.createElement('section');
@@ -21,7 +22,7 @@ describe("bespoke-keys", function() {
       }
 
       deck = bespoke.from(parent, [
-        keynav(opts),
+        keynav(),
         forms()
       ]);
     },
@@ -33,129 +34,92 @@ describe("bespoke-keys", function() {
       }
       deck = null;
     },
-    pressKey = function(which, isShift, element) {
-      simulant.fire((element || document), 'keydown', { which: which, shiftKey: !!isShift });
+    pressKey = function(which, opts, element) {
+      opts = opts ? opts : {};
+      opts.which = KEY[which] || which;
+      simulant.fire((element || document), 'keydown', opts);
     };
 
-  describe("horizontal deck", function() {
-    [undefined, 'horizontal'].forEach(function (opts) {
-      describe("with an option value of '" + opts + "'", function() {
-        beforeEach(createDeck.bind(null, opts));
-        afterEach(destroyDeck);
+  beforeEach(createDeck);
+  afterEach(destroyDeck);
 
-        describe("next slide", function() {
-          it("should go to the next slide when pressing the space bar", function() {
-            pressKey(32);
-            expect(deck.slide()).toBe(1);
-          });
+  describe('navigate to next slide', function() {
+    beforeEach(function() { deck.slide(0); });
 
-          it("should go to the next slide when pressing the right arrow", function() {
-            pressKey(39);
-            expect(deck.slide()).toBe(1);
-          });
+    it('should go to next slide when space bar is pressed', function() {
+      pressKey(KEY.spaceBar);
+      expect(deck.slide()).toBe(1);
+    });
 
-          it("should go to the next slide when pressing page down", function() {
-            pressKey(34);
-            expect(deck.slide()).toBe(1);
-          });
+    it('should go to next slide when right arrow is pressed', function() {
+      pressKey(KEY.right);
+      expect(deck.slide()).toBe(1);
+    });
 
-          it("should not go to the next slide when pressing the right arrow with shift pressed", function() {
-            pressKey(39, true);
-            expect(deck.slide()).toBe(0);
-          });
+    it('should go to next slide when page down is pressed', function() {
+      pressKey(KEY.pageDown);
+      expect(deck.slide()).toBe(1);
+    });
 
-          it("should not go to the next slide when pressing the space bar in an input field", function() {
-            pressKey(32, false, inputBox);
-            expect(deck.slide()).toBe(0);
-          });
-        });
+    it('should not go to next slide when right arrow and modifier key are pressed', function() {
+      pressKey(KEY.right, { shiftKey: true });
+      expect(deck.slide()).toBe(0);
+      pressKey(KEY.right, { ctrlKey: true });
+      expect(deck.slide()).toBe(0);
+    });
 
-        describe("previous slide", function() {
-          beforeEach(function() { deck.slide(1); });
-
-          it("should go to the previous slide when pressing the left arrow", function() {
-            pressKey(37);
-            expect(deck.slide()).toBe(0);
-          });
-
-          it("should go to the previous slide when pressing page up", function() {
-            pressKey(33);
-            expect(deck.slide()).toBe(0);
-          });
-
-          it("should go to the previous slide when pressing the shift and space bar", function() {
-            pressKey(32, true);
-            expect(deck.slide()).toBe(0);
-          });
-
-          it("should not go to the previous slide when pressing the left arrow with shift pressed", function() {
-            pressKey(37, true);
-            expect(deck.slide()).toBe(1);
-          });
-
-          it("should not go to the previous slide when pressing the shift and space bar in an input field", function() {
-            pressKey(32, true, inputBox);
-            expect(deck.slide()).toBe(1);
-          });
-        });
-
-        describe("last slide", function() {
-          beforeEach(function() { deck.slide(2); });
-
-          it("should go to the first slide when pressing home", function() {
-            pressKey(36);
-            expect(deck.slide()).toBe(0);
-          });
-        });
-
-        describe("first slide", function() {
-          it("should go to the last slide when pressing end", function() {
-            pressKey(35);
-            expect(deck.slide()).toBe(deck.slides.length - 1);
-          });
-        });
-      });
+    it('should not go to next slide when space bar is pressed in input field', function() {
+      pressKey(KEY.spaceBar, {}, inputBox);
+      expect(deck.slide()).toBe(0);
     });
   });
 
-  describe("vertical deck", function() {
-    beforeEach(createDeck.bind(null, 'vertical'));
-    afterEach(destroyDeck);
+  describe('navigate to previous slide', function() {
+    beforeEach(function() { deck.slide(1); });
 
-    describe("next slide", function() {
-      it("should go to the next slide when pressing the space bar", function() {
-        pressKey(32);
-        expect(deck.slide()).toBe(1);
-      });
-
-      it("should go to the next slide when pressing the down arrow", function() {
-        pressKey(40);
-        expect(deck.slide()).toBe(1);
-      });
-
-      it("should go to the next slide when pressing page down", function() {
-        pressKey(34);
-        expect(deck.slide()).toBe(1);
-      });
+    it('should go to previous slide when left arrow is pressed', function() {
+      pressKey(KEY.left);
+      expect(deck.slide()).toBe(0);
     });
 
-    describe("previous slide", function() {
-      beforeEach(function() { deck.slide(1); });
+    it('should go to previous slide when page up is pressed', function() {
+      pressKey(KEY.pageUp);
+      expect(deck.slide()).toBe(0);
+    });
 
-      it("should go to the previous slide when pressing the up arrow", function() {
-        pressKey(38);
-        expect(deck.slide()).toBe(0);
-      });
+    it('should go to previous slide when shift and space bar are pressed', function() {
+      pressKey(KEY.spaceBar, { shiftKey: true });
+      expect(deck.slide()).toBe(0);
+    });
 
-      it("should go to the previous slide when pressing page up", function() {
-        pressKey(33);
-        expect(deck.slide()).toBe(0);
-      });
+    it('should not go to previous slide when left arrow and modifier key are pressed', function() {
+      pressKey(KEY.left, { shiftKey: true});
+      expect(deck.slide()).toBe(1);
+      pressKey(KEY.left, { ctrlKey: true});
+      expect(deck.slide()).toBe(1);
+    });
 
-      it("should go to the previous slide when pressing the shift and space bar", function() {
-        pressKey(32, true);
-        expect(deck.slide()).toBe(0);
-      });
+    it('should not go to previous slide when shift and space bar are pressed in input field', function() {
+      pressKey(KEY.spaceBar, {}, inputBox);
+      expect(deck.slide()).toBe(1);
+    });
+  });
+
+  describe('navigate to first slide', function() {
+    beforeEach(function() { deck.slide(2); });
+
+    it('should go to first slide when home is pressed', function() {
+      pressKey(KEY.home);
+      expect(deck.slide()).toBe(0);
+    });
+  });
+
+  describe('navigate to last slide', function() {
+    beforeEach(function() { deck.slide(0); });
+
+    it('should go to last slide when end is pressed', function() {
+      pressKey(KEY.end);
+      expect(deck.slide()).toBe(deck.slides.length - 1);
     });
   });
 });
