f5f7d4e47a6d262053ac11eeb1c4849884b8db52
Dan Allen <dan.j.allen@gmail.com>
2016-02-12 01:31:35 -0700

    update demo styles

--- demo/demo.js
+++ demo/demo.js
@@ -2,5 +2,5 @@ bespoke.from('.deck', [
   bespoke.plugins.classes(),
   bespoke.plugins.navkbd(),
   bespoke.plugins.forms(),
-  bespoke.plugins.bullets('ul.step, ul.step > li')
+  bespoke.plugins.bullets('ul.build, ul.build > li')
 ]);
